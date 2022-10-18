d7edb5d8ea8893e5641e4d38b3991eaf3dc4abdb
Aiden Taylor <aiden.taylor@protonmail.com>
2022-10-18 08:14:53 -0600

    Initial commit.

---
+++ game/rules.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void rules() {
+
+  printf("\n\nRULES OF BIM:");
+  printf("\nThe aim of the game is to see who can reach 100 from 1 first, by "
+         "adding any number from 1 through 10 to a sum.");
+  printf("\nYou can type any number from 1 to 10 inclusive, and Bim will "
+         "choose another number.");
+
+  return;
+}
